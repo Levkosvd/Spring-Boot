30ebb40da6eae0a7975b48f575c0818f38eb561b
levko <levkosvd@gmail.com>
2020-03-01 23:15:47 +0200

    Create project

---
+++ src/main/java/mate/academy/boot/spring_boot_start/SpringBootStartApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.boot.spring_boot_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/boot/spring_boot_start/SpringBootStartApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.boot.spring_boot_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
